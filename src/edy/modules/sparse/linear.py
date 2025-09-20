c14ac360df5b7084ad9d5bfc387eb1e6136c3395
Arktetra <sameershrestha.sm@gmail.com>
2025-09-20 18:59:15 +0545

    add skeleton for sparse linear layer

---
+++ src/edy/modules/sparse/linear.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+from .tensor import SparseTensor
+
+class SparseLinear(nn.Linear):
+    def __init__(
+        self, in_features: int, out_features: int, bias: bool = True
+    ):
+        super(SparseLinear, self).__init__(in_features, out_features, bias)
+
+    def forward(
+        self, x: SparseTensor
+    ) -> SparseTensor:
+        raise NotImplementerError("Implement Me!")
