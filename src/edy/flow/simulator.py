7cb5a10a96abb557f6a7626502a409ef783bd89e
Arktetra <sameershrestha.sm@gmail.com>
2025-09-21 21:53:00 +0545

    add euler flow simulator

---
+++ src/edy/flow/simulator.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from tqdm import tqdm
+
+import torch
+
+from edy.flow.de import ODE
+
+class Simulator(ABC):
+    @abstractmethod
+    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor, **kwargs):
+        pass
+
+    @torch.no_grad()
+    def simulate(self, x: torch.Tensor, ts: torch.Tensor, **kwargs):
+        nts = ts.shape[1]
+        for t_idx in tqdm(range(nts - 1)):
+            t = ts[:, t_idx]
+            h = ts[:, t_idx + 1] - ts[:, t_idx]
+            x = self.step(x, t, h, **kwargs)
+        return x
+
+    @torch.no_grad()
+    def simulate_with_trajectory(self, x: torch.Tensor, ts: torch.Tensor, **kwargs):
+        xs = [x.clone()]
+        nts = ts.shape[1]
+        for t_idx in tqdm(range(nts - 1)):
+            t = ts[:, t_idx]
+            h = ts[:, t_idx + 1] - ts[:, t_idx]
+            x = self.step(x, t, h, **kwargs)
+            xs.append(x.clone())
+        return torch.stack(xs, dim=1)
+
+class EulerSimulator(Simulator):
+    def __init__(self, ode: ODE):
+        self.ode = ode
+
+    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor, **kwargs):
+        return xt + self.ode.drift_coefficient(xt, t, **kwargs) * dt
+    
