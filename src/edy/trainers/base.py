d4d58fd3409884910de6553036b4ff797476f85d
Arktetra <sameershrestha.sm@gmail.com>
2025-09-21 21:49:53 +0545

    add abstract base class for trainer

---
+++ src/edy/trainers/base.py
@@ -0,0 +1,34 @@
+
+import torch
+import torch.nn as nn
+
+from abc import ABC, abstractmethod
+
+from tqdm import tqdm
+
+class Trainer(ABC):
+    def __init__(self, model: nn.Module):
+        super().__init__()
+        self.model = model
+
+    @abstractmethod
+    def get_train_loss(self, **kwargs) -> torch.Tensor:
+        pass
+
+    def get_optimizer(self, lr: float):
+        return torch.optim.Adam(self.model.parameters(), lr=lr)
+
+    def train(self, num_epochs: int, device: torch.device, lr: float = 1e-3, **kwargs) -> torch.Tensor:
+        self.model.to(device)
+        opt = self.get_optimizer(lr)
+        self.model.train()
+
+        pbar = tqdm(enumerate(range(num_epochs)))
+        for idx, epoch in pbar:
+            opt.zero_grad()
+            loss = self.get_train_loss(**kwargs)
+            loss.backward()
+            opt.step()
+            pbar.set_description(f"Epoch {idx}, loss: {loss.item():.3f}")
+
+        self.model.eval()
