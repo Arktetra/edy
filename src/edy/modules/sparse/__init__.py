699b8c999345b1e149f2ebb9033a6210554fdfc0
Arktetra <sameershrestha.sm@gmail.com>
2025-11-20 18:59:16 +0545

    add sparse convolution

--- src/edy/modules/sparse/__init__.py
+++ src/edy/modules/sparse/__init__.py
@@ -1,3 +1,24 @@
-BACKEND = "spconv"
+from .tensor import SparseTensor
+
+__all__ = [
+    "SparseTensor",
+]
+
 DEBUG = False
-ATTN = "flash_attn"
+SPCONV_ALGO = 'auto'    # 'auto', 'implicit_gemm', 'native'
+
+def __from_env():
+    import os
+        
+    global SPCONV_ALGO
+    env_spconv_algo = os.environ.get('SPCONV_ALGO')
+    if env_spconv_algo is not None and env_spconv_algo in ['auto', 'implicit_gemm', 'native']:
+        SPCONV_ALGO = env_spconv_algo
+    print(f"[SPARSE][CONV] spconv algo: {SPCONV_ALGO}")
+        
+
+__from_env()
+
+def set_debug(debug: bool):
+    global DEBUG
+    DEBUG = debug
