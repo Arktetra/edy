8b21960534888ff96081e501c21f7acd139e1526
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-12-06 14:16:15 +0545

    update: radius and fov defined as constant (prev. direct numeric)

--- dataset_toolkits/render.py
+++ dataset_toolkits/render.py
@@ -9,6 +9,8 @@ from .blender_script.render import render
 from .utils import sphere_hammersley_sequence
 from .metadata.front_3d import RAW_DATA_DIR, PROCESSED_DATA_DIR
 
+SPHERE_RAIDUS = 2
+CAMERA_FOV_DEGREE = 40
 
 def handler(signal_received, frame):
     raise KeyboardInterrupt("SIGTERM received")
@@ -19,12 +21,11 @@ def _render(file_path, num_views):
     pitchs = []
     offset = (np.random.rand(), np.random.rand())
     for i in range(num_views):
-        # y, p = [0., 0.]
         y, p = sphere_hammersley_sequence(i, num_views, offset)
         yaws.append(y)
         pitchs.append(p)
-    radius = [2] * num_views
-    fov = [40 / 180 * np.pi] * num_views
+    radius = [SPHERE_RAIDUS] * num_views
+    fov = [CAMERA_FOV_DEGREE / 180 * np.pi] * num_views 
     views = [{"yaw": y, "pitch": p, "radius": r, "fov": f} for y, p, r, f in zip(yaws, pitchs, radius, fov)]
 
     print("launching blender")
