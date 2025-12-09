6b20053860ec44cbd6befc2cb69854fb113268de
Arktetra <sameershrestha.sm@gmail.com>
2025-12-09 18:07:39 +0545

    remove multiprocessing and add continuation

--- dataset_toolkits/render.py
+++ dataset_toolkits/render.py
@@ -1,12 +1,11 @@
-# from concurrent.futures import ProcessPoolExecutor
 import argparse
+import random
 import signal
 import numpy as np
 
 from pathlib import Path
 
 from .blender_script.render import render
-from .utils import sphere_hammersley_sequence
 from .metadata.front_3d import RAW_DATA_DIR, PROCESSED_DATA_DIR
 
 SPHERE_RAIDUS = 2
@@ -18,14 +17,15 @@ def handler(signal_received, frame):
 
 
 def _render(file_path, num_views):
-    yaws = []
-    pitchs = []
-    offset = (np.random.rand(), np.random.rand())
-    for i in range(num_views):
-        y, p = sphere_hammersley_sequence(i, num_views, offset)
-        # y, p = 0, np.pi / 180 * 20
-        yaws.append(y)
+    yaws = [0]
+    pitchs = [np.pi / 6]
+    base_y = random.uniform(0, 2 * np.pi)
+    for i in range(num_views - 1):
+        p = random.uniform(np.pi / 9, np.pi / 4)
+
+        yaws.append(base_y + 2 * i * np.pi / 3)
         pitchs.append(p)
+
     radius = [SPHERE_RAIDUS] * num_views
     fov = [CAMERA_FOV_DEGREE / 180 * np.pi] * num_views
     views = [{"yaw": y, "pitch": p, "radius": r, "fov": f} for y, p, r, f in zip(yaws, pitchs, radius, fov)]
@@ -48,7 +48,6 @@ if __name__ == "__main__":
         prog="render", description="Render the model in the raw directory to obtain scenes from different views."
     )
     parser.add_argument("--num-views", type=int, default=1, help="Number of views to render")
-    parser.add_argument("--max-workers", type=int, default=1, help="Maximum number of workers")
     parser.add_argument("--save-mask", action="store_true", help="create masks for rendered images.")
     args = parser.parse_args()
     Path.mkdir(PROCESSED_DATA_DIR, parents=True, exist_ok=True)
@@ -56,11 +55,7 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, handler)
 
     input_paths = list(RAW_DATA_DIR.glob("**/*"))
+
     for path in input_paths:
-        _render(path, 4)
-    # _render(Path(__file__).parents[1] / "temp" / "767.glb", 1)
-    # with ProcessPoolExecutor(max_workers=args.max_workers) as executor:
-    #     try:
-    #         executor.map(_render, input_paths, [args.num_views] * len(input_paths))
-    #     except KeyboardInterrupt:
-    #         print("Keyboard interrupt occurred.")
+        if not (PROCESSED_DATA_DIR / "models" / f"{path.stem}.ply").exists():
+            _render(path, args.num_views)
