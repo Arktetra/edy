3d6d3e14b2d5cfb22904ae2b83ba47142e40c04f
Arktetra <sameershrestha.sm@gmail.com>
2025-12-23 11:22:20 +0545

    add position head

--- src/edy/modules/position_head.py
+++ src/edy/modules/position_head.py
@@ -1,26 +1,26 @@
-import math
-import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.utils.checkpoint import checkpoint
 
-from edy.modules.transformer.modulated import ModulatedTransformerBlock, ModulatedTransformerCrossBlock
+from edy.modules.norm import LayerNorm32
+from edy.modules.transformer.modulated import ModulatedTransformerBlock
+
 
 def activate_pose(pred_pose_enc, trans_act="linear", quat_act="linear", s_act="linear"):
     t = pred_pose_enc[..., :3]
-    quat = pred_pose_enc[..., 3:6]
-    scale = pred_pose_enc[..., 6:]
+    quat = pred_pose_enc[..., 3:7]
+    scale = pred_pose_enc[..., 7:]
 
     t = base_pose_act(t, trans_act)
-    quat = base_pose_act(t, quat_act)
-    s = base_pose_act(t, s_act)
+    quat = base_pose_act(quat, quat_act)
+    scale = base_pose_act(scale, s_act)
 
-    pred_pose_enc = torch.cat([t, quat, s], dim=-1)
+    pred_pose_enc = torch.cat([t, quat, scale], dim=-1)
 
     return pred_pose_enc
+
 
 def base_pose_act(pose_enc, act_type="linear"):
     if act_type == "linear":
@@ -34,8 +34,10 @@ def base_pose_act(pose_enc, act_type="linear"):
     else:
         raise ValueError(f"Unknown act_type: {act_type}")
 
+
 def inverse_log_transform(y):
     return torch.sign(y) * (torch.expm1(torch.abs(y)))
+
 
 class PositionHead(nn.Module):
     def __init__(
@@ -49,10 +51,10 @@ class PositionHead(nn.Module):
         quat_act: str = "linear",
         s_act: str = "relu",
         use_checkpoint: bool = False,
-        use_fp16: bool = False
+        use_fp16: bool = False,
     ):
         super(PositionHead, self).__init__()
-        
+
         if pose_encoding_type == "absT_quatR_S":
             self.target_dim = 8
         elif pose_encoding_type == "absT_eulerR_S":
@@ -78,20 +80,19 @@ class PositionHead(nn.Module):
                     use_rope=False,
                     qk_rms_norm=True,
                     qkv_bias=True,
-                    share_mod=False
-
+                    share_mod=False,
                 )
                 for _ in range(trunk_depth)
             ]
         )
 
-        self.token_norm = nn.LayerNorm(model_channels, elementwise_affine=False, eps=1e-6)
-        self.trunk_norm = nn.LayerNorm(model_channels, elementwise_affine=False, eps=1e-6)
+        self.token_norm = LayerNorm32(model_channels, elementwise_affine=False, eps=1e-6)
+        self.trunk_norm = LayerNorm32(model_channels, elementwise_affine=False, eps=1e-6)
 
         self.empty_pose_tokens = nn.Parameter(torch.zeros(1, self.target_dim))
         self.embed_pose = nn.Linear(self.target_dim, model_channels)
 
-        self.adaln_norm = nn.LayerNorm(model_channels, elementwise_affine=False, eps=1e-6)
+        self.adaln_norm = LayerNorm32(model_channels, elementwise_affine=False, eps=1e-6)
         self.pose_branch = nn.Sequential(
             nn.Linear(model_channels, model_channels // 2, bias=True),
             nn.GELU(),
@@ -106,9 +107,9 @@ class PositionHead(nn.Module):
                 if m.bias is not None:
                     nn.init.normal_(m.bias, 0, 1e-2)
             elif isinstance(m, nn.LayerNorm):
-                if hasattr(m, 'weight') and m.weight is not None:
+                if hasattr(m, "weight") and m.weight is not None:
                     nn.init.normal_(m.weight, 1.0, 1e-2)
-                if hasattr(m, 'bias') and m.bias is not None:
+                if hasattr(m, "bias") and m.bias is not None:
                     nn.init.normal_(m.bias, 0, 1e-2)
 
         nn.init.zeros_(self.empty_pose_tokens)  # Translation
@@ -116,7 +117,55 @@ class PositionHead(nn.Module):
         nn.init.ones_(self.empty_pose_tokens[0, 7])  # Scale
 
     def _forward(self, position_token: torch.Tensor, num_iteration: int = 4):
-        raise NotImplementedError("implement me!")
+        position_token = self.token_norm(position_token)
+
+        B, N, C = position_token.shape
+        assert N == 1, "Position head only supports single token input"
+        pred_pose_enc = None
+
+        for _ in range(num_iteration):
+            if pred_pose_enc is None:
+                module_input = self.embed_pose(self.empty_pose_tokens.expand(B, -1))
+            else:
+                module_input = self.embed_pose(pred_pose_enc.squeeze(1))
+
+            position_token_block = position_token.clone()
+            position_token_block = position_token_block.type(self.dtype)
+            module_input = module_input.type(self.dtype)
+            for block in self.trunk:
+                position_token_block = block(
+                    position_token_block,
+                    module_input,
+                )
+            position_token_block = position_token_block.type(position_token.dtype)
+
+            pred_pose_enc_delta = self.pose_branch(self.trunk_norm(position_token_block))
+
+            if pred_pose_enc is None:
+                pred_pose_enc = pred_pose_enc_delta
+            else:
+                pred_pose_enc = pred_pose_enc + pred_pose_enc_delta
+
+            activated_pose = activate_pose(
+                pred_pose_enc,
+                trans_act=self.trans_act,
+                quat_act=self.quat_act,
+                s_act=self.s_act,
+            )
+
+        if activated_pose.ndim == 3:
+            activated_pose = activated_pose.squeeze(1)
+        if not self.training:
+            if self.pose_encoding_type == "absT_eulerR_S":
+                activated_pose[0, ...] = torch.tensor(
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], device=activated_pose.device
+                ).type(activated_pose.dtype)
+            elif self.pose_encoding_type == "absT_quatR_S":
+                activated_pose[0, ...] = torch.tensor(
+                    [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0], device=activated_pose.device
+                ).type(activated_pose.dtype)
+
+        return activated_pose
 
     def forward(self, position_token: torch.Tensor, num_iteration: int = 4):
         if self.use_checkpoint:
