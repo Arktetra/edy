4f6fafcbbe850dd23c25556b8174b758e8eaaed1
Arktetra <sameershrestha.sm@gmail.com>
2026-01-05 22:12:22 +0545

    update extractor script

--- dataset_toolkits/blender_script/3D-Front/extractor.py
+++ dataset_toolkits/blender_script/3D-Front/extractor.py
@@ -5,37 +5,51 @@ from .utils import *
 from .transforms import *
 from .objects import *
 
+from ...metadata.front_3d import RAW_DATA_DIR
+from ...metadata.edy import PROCESSED_DATA_DIR
+
 import argparse
 import os
 from pathlib import Path
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="extractor", description="Designed to extract transform prop. of all mesh/object in scene (of dataset) & export mesh individually"
+        prog="extractor",
+        description="Designed to extract transform prop. of all mesh/object in scene (of dataset) & export mesh individually",
     )
 
-    cwd = Path(os.getcwd())
+    # cwd = Path(os.getcwd())
 
-    parser.add_argument("--dataset-dir", type=str, default=str(cwd / "dataset"), help="Path where the dataset exists")
-    parser.add_argument("--output-dir", type=str, default=str(cwd / "output"), help="Where the output will be saved")
-    parser.add_argument("--ds-filter-ext", nargs='+', default=["glb"], help="dataset extension format which should only be taken from dataset folder")
+    # parser.add_argument("--dataset-dir", type=str, default=str(cwd / "dataset"), help="Path where the dataset exists")
+    # parser.add_argument("--output-dir", type=str, default=str(cwd / "output"), help="Where the output will be saved")
+    parser.add_argument(
+        "--ds-filter-ext",
+        nargs="+",
+        default=["glb"],
+        help="dataset extension format which should only be taken from dataset folder",
+    )
 
-    parser.add_argument("--objects", action='store_true', help="Should extract objects?")
-    parser.add_argument("--transforms", action='store_true', help="Should extract objects transform info?")
-    parser.add_argument("--at-once", action='store_true', help="Should extract transform and object at single load? or seperately.")
-    parser.add_argument("--all", action='store_true', help="Extract all the available infos?")
+    parser.add_argument("--objects", action="store_true", help="Should extract objects?")
+    parser.add_argument("--transforms", action="store_true", help="Should extract objects transform info?")
+    parser.add_argument(
+        "--at-once", action="store_true", help="Should extract transform and object at single load? or seperately."
+    )
+    parser.add_argument("--all", action="store_true", help="Extract all the available infos?")
 
     args = parser.parse_args()
 
-    dataset_dir, output_dir = Path(args.dataset_dir), Path(args.output_dir)
-    if not os.path.exists(dataset_dir):
-        raise FileNotFoundError(f"Given dataset dir '{dataset_dir}' not found!!")
+    # dataset_dir, output_dir = Path(args.dataset_dir), Path(args.output_dir)
+    # # print(output_dir)
 
-    if os.path.exists(output_dir):
-        os.system(f"rm -rf {output_dir}")
-        print("*** Warn: Output path already exists override it!!! ***")
-    
-    os.makedirs(output_dir, exist_ok=True)
+    # if not os.path.exists(dataset_dir):
+    #     raise FileNotFoundError(f"Given dataset dir '{dataset_dir}' not found!!")
+
+    # if os.path.exists(output_dir):
+    #     os.system(f"rm -rf {output_dir}")
+    #     print("*** Warn: Output path already exists override it!!! ***")
+
+    # os.makedirs(output_dir, exist_ok=True)
+    dataset_dir, output_dir = RAW_DATA_DIR, PROCESSED_DATA_DIR
 
     all, at_once = args.all, args.at_once
     objects = True if all else args.objects
@@ -45,18 +59,22 @@ if __name__ == "__main__":
     scene_paths = [item for item in os.listdir(dataset_dir) if item.split(".")[-1] in ext_filter]
 
     # print(args)
+    # print(scene_paths)
 
     # function pointer for importer and exporter
     obj_importer = bpy.ops.import_scene.gltf
     obj_exporter = bpy.ops.wm.ply_export
-    exporter_ext_format = "ply" # for more flexible for sudden export format change..., warning: match the exporter and its extension
+    exporter_ext_format = (
+        "ply"  # for more flexible for sudden export format change..., warning: match the exporter and its extension
+    )
 
     # create the folders for the transforms and objects before hand..
     trans_out_dir = output_dir / "transforms"
     obj_out_dir = output_dir / "objects"
-    os.makedirs(trans_out_dir)
-    os.makedirs(obj_out_dir)
-        
+
+    Path.mkdir(trans_out_dir, parents=True, exist_ok=True)
+    Path.mkdir(obj_out_dir, parents=True, exist_ok=True)
+
     if at_once:
         # transform and object extraction simulataneously in single load..
         for scene_path in scene_paths:
