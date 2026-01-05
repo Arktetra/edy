03047b573f534250a058e71b533f3d6a98a8ae8c
Arktetra <sameershrestha.sm@gmail.com>
2026-01-05 22:16:38 +0545

    add metadata

---
+++ src/edy/metadata/common.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parents[3]
+DATA_DIR = ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+if __name__ == "__main__":
+    print(DATA_DIR)
