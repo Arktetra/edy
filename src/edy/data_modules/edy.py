a9285566674c9411bacf1e3c775ebdeb7bd91caa
Arktetra <sameershrestha.sm@gmail.com>
2026-01-06 21:51:23 +0545

    add parameter for root

--- src/edy/data_modules/edy.py
+++ src/edy/data_modules/edy.py
@@ -1,16 +1,18 @@
 from edy.data_modules.base import DataModule
 from edy.datasets.edy_dataset import EdyDataset
 from edy.metadata.edy import PROCESSED_DATA_DIR
-from edy.metadata.common import ROOT
 
 import huggingface_hub
 import zipfile
 
 
 class EdyDataModule(DataModule):
-    def __init__(self, batch_size: int = 1, shuffle: bool = True, num_workers: int = 1, on_gpu: bool = False):
+    def __init__(
+        self, root: str, batch_size: int = 1, shuffle: bool = True, num_workers: int = 1, on_gpu: bool = False
+    ):
         super().__init__(batch_size, shuffle, num_workers, on_gpu)
         self.data_dir = PROCESSED_DATA_DIR
+        self.root = root
 
     def prepare_data(self):
         if self.data_dir.exists():
@@ -20,10 +22,10 @@ class EdyDataModule(DataModule):
         huggingface_hub.snapshot_download(
             repo_id="Darktetra/edy-dataset-final",
             repo_type="dataset",
-            local_dir=ROOT,
+            local_dir=self.root,
         )
 
-        with zipfile.ZipFile(ROOT / "edy.zip") as ref:
+        with zipfile.ZipFile(f"{self.root}/edy.zip") as ref:
             ref.extractall()
 
     def setup(self):
