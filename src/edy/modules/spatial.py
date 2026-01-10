5cd70d64fc3ce8e828715d307d670c351d91932a
Arktetra <sameershrestha.sm@gmail.com>
2026-01-10 12:39:44 +0545

    add patchiy and unpatchify

--- src/edy/modules/spatial.py
+++ src/edy/modules/spatial.py
@@ -25,7 +25,7 @@ def pixel_shuffle_3d(x: Float[torch.Tensor, "B C D H W"], scale_factor: int) -> 
 
 def patchify(
     x: Float[torch.Tensor, "B C D H W"],
-    p: int,  # patch size
+    patch_size: int,  # patch size
 ) -> Float[torch.Tensor, "B C*(p**3) D//p H//p W//p"]:
     """
     Split a tensor into non-overlapping patches, by subdividing each spatial dimension into blocks of size `p`, and then re-encoding those patches into the channel dimension.
@@ -36,18 +36,33 @@ def patchify(
             B = batch size,
             C = number of channels,
             D, H, W = spatial dimensions
-        p (int): a positive integer called patch size that exactly divides D, H, and W.
+        patch_size (int): a positive integer called patch size that exactly divides D, H, and W.
 
     Returns:
     -----
         torch.Tensor: a tensor of shape (B, C*(p**3), D//p, H//p, W//p)
     """
-    raise NotImplementedError("Implement Me!")
+    DIM = x.dim() - 2
+    for d in range(2, DIM + 2):
+        assert x.shape[d] % patch_size == 0, (
+            f"Dimension {d} of input tensor must be divisible by patch size, got {x.shape[d]} and {patch_size}"
+        )
+
+    # [B, C, D, H, W] -> [B, C, D//p, p, H//p, p, W//p, p]
+    x = x.reshape(*x.shape[:2], *sum([[x.shape[d] // patch_size, patch_size] for d in range(2, DIM + 2)], []))
+
+    # [B, C, D//p, p, H//p, p, W//p, p] -> [B, C, p, p, p, D//p, H//p, W//p]
+    x = x.permute(0, 1, *([2 * i + 3 for i in range(DIM)] + [2 * i + 2 for i in range(DIM)]))
+
+    # [B, C, p, p, p, D//p, H//p, W//p] -> [B, C * (p ** 3), D//p, H//p, W//p]
+    x = x.reshape(x.shape[0], x.shape[1] * (patch_size**DIM), *(x.shape[-DIM:]))
+
+    return x
 
 
 def unpatchify(
     x: Float[torch.Tensor, "B C D H W"],
-    p: int,
+    patch_size: int,
 ) -> Float[torch.Tensor, "B C//(p**3) D*p H*p W*p"]:
     """
     Reverse the patchification process by rearranging the patch dimensions back into spatial dimensions.
@@ -58,10 +73,18 @@ def unpatchify(
             B = batch size,
             C = number of channels (must be divisible by p^3),
             D, H, W = spatial dimensions
-        p (int): a positive integer called patch size.
+        patch_size (int): a positive integer called patch size.
 
     Returns:
     -----
         torch.Tensor: reconstructed tensor of shape (B, C//(p**3), D*p, H*p, W*p).
     """
-    raise NotImplementedError("Implement Me!")
+    DIM = x.dim() - 2
+    assert x.shape[1] % (patch_size**DIM) == 0, (
+        f"Second dimension of input tensor must be divisible by patch size to unpatchify, got {x.shape[1]} and {patch_size**DIM}"
+    )
+
+    x = x.reshape(x.shape[0], x.shape[1] // (patch_size**DIM), *([patch_size] * DIM), *(x.shape[-DIM:]))
+    x = x.permute(0, 1, *(sum([[2 + DIM + i, 2 + i] for i in range(DIM)], [])))
+    x = x.reshape(x.shape[0], x.shape[1], *[x.shape[2 + 2 * i] * patch_size for i in range(DIM)])
+    return x
