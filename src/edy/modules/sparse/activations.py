901ebd8b8351dd7522b765bdcdc869971f1a6dd7
Arktetra <sameershrestha.sm@gmail.com>
2026-01-20 17:02:18 +0545

    add sparse activation functions

--- src/edy/modules/sparse/activations.py
+++ src/edy/modules/sparse/activations.py
@@ -1,24 +1,27 @@
-import torch
 import torch.nn as nn
 
 from edy.modules.sparse.tensor import SparseTensor
 
+
 class SparseReLU(nn.ReLU):
     def forward(self, x: SparseTensor) -> SparseTensor:
-        raise NotImplementedError("Implement Me!")
+        return x.replace(super().forward(x.feats))
+
 
 class SparseSiLU(nn.ReLU):
     def forward(self, x: SparseTensor) -> SparseTensor:
-        raise NotImplementedError("Implement Me!")
+        return x.replace(super().forward(x.feats))
+
 
 class SparseGELU(nn.GELU):
     def forward(self, x: SparseTensor) -> SparseTensor:
-        raise NotImplementedError("Implement Me!")
+        return x.replace(super().forward(x.feats))
+
 
 class SparseActivation(nn.Module):
     def __init__(self, activation: nn.Module):
         super().__init__()
         self.activation = activation
 
-    def forward(self, input: SparseTensor) -> SparseTensor:
-        raise NotImplementedError("Implement Me!")
+    def forward(self, x: SparseTensor) -> SparseTensor:
+        return x.replace(self.activation(x.feats))
