87fcd4b5b76709627377fe27dcf277a0e765f054
Arktetra <sameershrestha.sm@gmail.com>
2026-01-20 17:00:41 +0545

    add implementation for sparse linear

--- src/edy/modules/sparse/linear.py
+++ src/edy/modules/sparse/linear.py
@@ -1,15 +1,11 @@
-import torch
 import torch.nn as nn
 
-from .tensor import SparseTensor
+from edy.modules.sparse.tensor import SparseTensor
+
 
 class SparseLinear(nn.Linear):
-    def __init__(
-        self, in_features: int, out_features: int, bias: bool = True
-    ):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True):
         super(SparseLinear, self).__init__(in_features, out_features, bias)
 
-    def forward(
-        self, x: SparseTensor
-    ) -> SparseTensor:
-        raise NotImplementedError("Implement Me!")
+    def forward(self, x: SparseTensor) -> SparseTensor:
+        return x.replace(super().forward(x.feats))
